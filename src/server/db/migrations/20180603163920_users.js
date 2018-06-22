3d159e6311ecac1247944037f280d917c3219e53
mariovegabarbas <mario.vega.barbas@ki.se>
2018-06-22 14:15:50 +0200

    last with atos

--- src/server/db/migrations/20180603163920_users.js
+++ src/server/db/migrations/20180603163920_users.js
@@ -5,6 +5,11 @@ exports.up = function(knex, Promise) {
     table.string('password').notNullable();
     table.string('accesstoken').unique();
     table.string('userid').unique();
+
+    /* To review and add to the sytem: This token has to be used 
+    to sent notifications to the user mobile */
+    //table.string('mphtoke').unique();
+    
     table.boolean('admin').notNullable().defaultTo(false);
   });
 };
--- src/server/routes/notifications.js
+++ src/server/routes/notifications.js
@@ -56,4 +56,41 @@ router.get(BASE_URL, async (ctx) =>{
 	}
 });
 
+/*router.get(`${BASE_URL}/mph`, async (ctx) =>{
+	if (helpers.ensureAuthenticated(ctx)) {
+    	try {
+      		fetch('http://192.168.43.116:8080/api/send', {
+				method: 'POST',
+			    headers: {
+			    	'Content-Type': 'application/json',
+			    },
+			    body: "{'title': 'title test', 'body': 'Hej Hej!', 'user': 'nexus-pocket-waw'}"
+			})*/
+			  /** Inspect this section **//*
+			  .then(function(res) {
+			      if (res.status !== 200){
+			          console.log(res.status);
+			          return null;
+			      }else{
+			          return res.json();
+			      }
+			    })
+			    .then(function(body) {
+			        console.log(body);
+			      }).catch(function(err){
+			        console.log(err);
+			      });
+      		ctx.body = {
+      			id_user: helpers.getIdUser(ctx),
+        		title: `Polar Notification for all sensing period`,
+        		message: aux
+      		};
+	    } catch (err) {
+	      console.log(err)
+	    }
+	} else {
+		ctx.redirect('/auth/login');
+	}
+});*/
+
 module.exports = router;
